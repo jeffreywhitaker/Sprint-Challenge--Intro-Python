66572e3b90afbf1d3653017260004c05991cecbb
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-01-17 11:10:41 -0800

    fix super() typo ln 28 oop2.py

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -25,7 +25,7 @@ class GroundVehicle():
 
 class Motorcycle(GroundVehicle):
     def __init__(self, num_wheels=2):
-        super()
+        super().__init__(num_wheels)
 
     def drive(self):
         return "BRAAAP!!"
